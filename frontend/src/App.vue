89cf20c6b6bb61e264955b13d0ff825214ae3ae4
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-24 16:02:28 +0200

    install laravel and vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/base/reset";
+@import "@/assets/scss/base/typo";
+@import "@/assets/scss/base/base";
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+</style>
