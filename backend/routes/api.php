3c9b4367e30247470e75874230327d24b130b0f0
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-26 20:21:52 +0200

    first migration

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,17 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/v1/images', [ImageController::class, 'index'])->name('allImages');
+Route::get('/v1/images/{id}', [ImageController::class, 'index'])->name('oneImage');
+Route::post('/v1/images/{id}', [ImageController::class, 'create'])->name('createImage');
+Route::put('/v1/images/{id}', [ImageController::class, 'update'])->name('updateImage');
+Route::delete('/v1/images/{id}', [ImageController::class, 'delete'])->name('deleteImage');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
