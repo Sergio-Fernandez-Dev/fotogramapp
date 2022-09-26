ac2f913f64886a4306fe19cf8abc639f2b949bbc
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-26 22:50:24 +0200

    change naming

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/images', [ImageController::class, 'index'])->name('allImages');
-Route::get('/images/{id}', [ImageController::class, 'index'])->name('oneImage');
-Route::post('/images/{id}', [ImageController::class, 'create'])->name('createImage');
-Route::put('/images/{id}', [ImageController::class, 'update'])->name('updateImage');
-Route::delete('/images/{id}', [ImageController::class, 'delete'])->name('deleteImage');
+Route::apiResource('images', ImageController::class);
 
 
--- backend/tests/Feature/ImagesIndexRouteTest.php
+++ backend/tests/Feature/ImagesIndexRouteTest.php
@@ -7,25 +7,27 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AllImagesRouteTest extends TestCase
+class ImagesIndexRouteTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_200_status_is_received() {
+    public function test_200_status_is_received()
+    {
 
         $this->withExceptionHandling();
 
         Image::factory(15)->create();
-        $response = $this->get(\route('allImages'));
+        $response = $this->get(\route('images.index'));
         $response->assertStatus(200);
     }
 
-    public function test_all_images_are_received() {
+    public function test_all_images_are_received()
+    {
 
         $this->withExceptionHandling();
 
         Image::factory(15)->create();
-        $response = $this->get(\route('allImages'));
+        $response = $this->get(\route('images.index'));
         $response->assertJsonCount(15);
     }
 }
