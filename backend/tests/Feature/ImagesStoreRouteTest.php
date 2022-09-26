16ffc7ee632d84c45eb1c8ca90eb3ef118399214
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-26 23:02:25 +0200

    create image store route test

---
+++ backend/tests/Feature/ImagesStoreRouteTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Image;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ImagesStoreRouteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_200_status_is_received()
+    {
+
+        $this->withExceptionHandling();
+
+        $request = Image::factory()->create();
+        $response = $this->post(\route('images.store'), $request);
+        $response->assertStatus(200);
+    }
+
+    public function test_new_image_is_stored()
+    {
+
+        $this->withExceptionHandling();
+
+        $request = Image::factory()->create();
+        $this->post(\route('images.store'), $request);
+        $this->assertCount(1, Image::all());
+    }
+}
