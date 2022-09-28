a3ee41faaa2f637f5a30bf63a32acc80756bc89a
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-09-28 18:56:23 +0200

    change url db field to nullable

--- backend/database/migrations/2022_09_26_180026_create_images_table.php
+++ backend/database/migrations/2022_09_26_180026_create_images_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('url');
+            $table->string('url')->nullable();
             $table->timestamps();
         });
     }
